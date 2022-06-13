f2ebff73db0f738f893f974fd8b2f6e94f681141
Marcin <marcincabaj94@gmail.com>
2022-06-13 20:54:04 +0200

    changed maplibre version

--- .eslintrc.js
+++ .eslintrc.js
@@ -22,6 +22,7 @@ module.exports = {
     ],
     "rules": {
         "no-unused-vars": "off",
-        "@typescript-eslint/no-unused-vars": "error"
+        "@typescript-eslint/no-unused-vars": "error",
+        "react/prop-types": "off",
     }
 }
